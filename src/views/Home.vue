3aa5f3142456b99005128dc65ac9a4c560e4c88b
4m1g0 <dev.4m1g0@gmail.com>
2021-02-09 05:09:05 +0100

    Include map in the home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="home">
-    This is the home
+    <div class="map" style="height:70%">
+        <iframe src="https://api.tinygs.com/worldmap/" style="height:100%;width:100%;" ></iframe>
+    </div>
+    <v-container>
+      <h2 class="grey--text text--darken-1">Welcome to TinyGS, the Open Source Global Satellite Network</h2>
+    <p></p>
+    </v-container>
+      
   </div>
 </template>
 
@@ -14,3 +21,13 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.home {
+  height: 100%;
+}
+
+h2 {
+  text-align: center;
+}
+</style>
