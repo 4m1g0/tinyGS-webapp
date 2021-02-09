031039b21892e0a4718e85e340b0ac7bf5eefbea
4m1g0 <dev.4m1g0@gmail.com>
2021-02-09 03:27:02 +0100

    Build skeleton and base style

---
+++ src/components/Navbar.vue
@@ -0,0 +1,36 @@
+<template>
+  <nav>
+    <v-app-bar app color="primary" dark height="70">
+      <v-app-bar-nav-icon @click="drawer=!drawer" class="mr-4"></v-app-bar-nav-icon>
+
+      <div class="flex-grow-1 d-flex justify-center">
+        <img alt="TinyGS logo" src="../assets/logo.png">
+      </div>
+
+      <v-btn color="gray" text>
+        <span>Sign Out</span>
+        <v-icon right>mdi-exit-to-app</v-icon>
+      </v-btn>
+      <Login />
+    </v-app-bar >
+    <v-navigation-drawer v-model="drawer" app color="primary" class="darken-1">
+      <p>test</p>
+    </v-navigation-drawer>
+  </nav>
+</template>
+
+<script>
+
+
+export default {
+  data() {
+    return {
+      drawer: false
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
