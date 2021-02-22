862af004b8171e02d40f45a1d1eb0e5be5d5ea2e
4m1g0 <dev.4m1g0@gmail.com>
2021-02-22 17:51:42 +0100

    Move station to a component

--- src/views/Stations.vue
+++ src/views/Stations.vue
@@ -3,40 +3,8 @@
     <v-container>
       <v-layout row wrap v-if="stations">
         <v-flex xs12 sm12 pa-4>
-          <div v-for="(station, i) in stations" :key="i"> 
-            <v-card flat :class="`pa-7 clickable ${(station.status ? 'online' : 'offline')}`" :to="`/station/${station.name}@${station.userId}`">
-              <v-layout row wrap>
-                <v-flex xs12 sm12 md12 lg3 xl2>
-                  <h3 class="caption grey--text">Station Name</h3>
-                  <div ><strong>{{station.name}}</strong></div>
-                </v-flex>
-                <v-flex xs6 sm4 md3 lg2 xl1>
-                  <div class="caption grey--text text-center">Listening</div>
-                  <div class="text-center">{{station.satellite}}</div>
-                </v-flex>
-                <v-flex xs6 sm4 md3 lg1 xl1>
-                  <div class="caption grey--text text-center">Version</div>
-                  <div class="text-center">{{station.version}}</div>
-                </v-flex>
-                <v-flex xs6 sm4 md3 lg1 xl1>
-                  <div class="caption grey--text text-center">Last seen</div>
-                  <div class="text-center">{{dateSince(station.lastSeen)}}</div>
-                </v-flex>
-                <v-flex xs6 sm4 md3 lg1 xl1>
-                  <div class="caption grey--text text-center">Auto tunning</div>
-                  <div class="text-center"><v-icon>{{station.remoteTune?"mdi-check":"mdi-cancel"}}</v-icon></div>
-                </v-flex>
-                <v-flex xs6 sm4 md3 lg1 xl1>
-                  <div class="caption grey--text text-center">Test mode</div>
-                  <div class="text-center">{{station.test?"ON":"OFF"}}</div>
-                </v-flex>
-                <v-flex xs6 sm4 md3 lg1 xl1>
-                  <div class="caption grey--text text-center">Confirmed packets</div>
-                  <div class="text-center">{{station.confirmedPackets}}</div>
-                </v-flex>
-                
-              </v-layout>
-            </v-card>
+          <div v-for="station in stations" :key="`${station.name}@${station.userId}`"> 
+            <Station :station="station"/>
             <v-divider></v-divider>
           </div>
         </v-flex>
@@ -50,12 +18,12 @@
 
 <script>
 const axios = require("axios");
-import moment from 'moment'
+import Station from '../components/Station.vue'
 
 export default {
   name:"Stations",
   components: {
-
+    Station
   },
   data() {
     return {
@@ -70,22 +38,13 @@ export default {
       const { data } = await axios.get("https://api.tinygs.com/v1/stations");
       console.log(data);
       this.stations = data;
-    },
-    dateSince(time) {
-      return moment(time).fromNow()
-    },
+    }
   }
   
 }
 </script>
 
 <style>
-.online {
-  border-left: 4px solid green !important;
-}
 
-.offline {
-  border-left: 4px solid red !important;
-}
 
 </style>
--- src/views/User.vue
+++ src/views/User.vue
@@ -24,40 +24,8 @@
   
       <!-- Stations -->
       <v-flex xs12 sm12 pa-4>
-        <div v-for="(station, i) in stations" :key="i"> 
-          <v-card flat :class="`pa-7 clickable ${(station.status ? 'online' : 'offline')}`" :to="`/station/${station.name}@${station.userId}`">
-            <v-layout row wrap>
-              <v-flex xs12 sm12 md12 lg3 xl2>
-                <h3 class="caption grey--text">Station Name</h3>
-                <div ><strong>{{station.name}}</strong></div>
-              </v-flex>
-              <v-flex xs6 sm4 md3 lg2 xl1>
-                <div class="caption grey--text text-center">Listening</div>
-                <div class="text-center">{{station.satellite}}</div>
-              </v-flex>
-              <v-flex xs6 sm4 md3 lg1 xl1>
-                <div class="caption grey--text text-center">Version</div>
-                <div class="text-center">{{station.version}}</div>
-              </v-flex>
-              <v-flex xs6 sm4 md3 lg1 xl1>
-                <div class="caption grey--text text-center">Last seen</div>
-                <div class="text-center">{{dateSince(station.lastSeen)}}</div>
-              </v-flex>
-              <v-flex xs6 sm4 md3 lg1 xl1>
-                <div class="caption grey--text text-center">Auto tunning</div>
-                <div class="text-center"><v-icon>{{station.remoteTune?"mdi-check":"mdi-cancel"}}</v-icon></div>
-              </v-flex>
-              <v-flex xs6 sm4 md3 lg1 xl1>
-                <div class="caption grey--text text-center">Test mode</div>
-                <div class="text-center"><v-icon>{{station.test?"mdi-alert-outline":""}}</v-icon></div>
-              </v-flex>
-              <v-flex xs6 sm4 md3 lg1 xl1>
-                <div class="caption grey--text text-center">Confirmed packets</div>
-                <div class="text-center">{{station.confirmedPackets}}</div>
-              </v-flex>
-              
-            </v-layout>
-          </v-card>
+        <div v-for="station in stations" :key="`${station.name}@${station.userId}`"> 
+          <Station :station="station"/>
           <v-divider></v-divider>
         </div>
       </v-flex>
@@ -72,9 +40,12 @@
 
 <script>
 const axios = require("axios");
-import moment from 'moment'
+import Station from '../components/Station.vue'
 
 export default {
+  components: {
+    Station
+  },
   data() {
     return {
       user: true,
@@ -92,21 +63,11 @@ export default {
       const { data } = await axios.get(`https://api.tinygs.com/v1/stations?userId=${localStorage.userId}`);
       console.log(data);
       this.stations = data;
-    },
-    dateSince(time) {
-      return moment(time).fromNow()
-    },
+    }
   }
 }
 </script>
 
 <style>
-.online {
-  border-left: 4px solid green !important;
-}
-
-.offline {
-  border-left: 4px solid red !important;
-}
 
 </style>
