d863be50f6be0ac25022ec2fd90d532cf22bf96a
4m1g0 <dev.4m1g0@gmail.com>
2021-05-11 01:58:45 +0200

    Restructure satellite to better fit the columns

--- src/views/Satellite.vue
+++ src/views/Satellite.vue
@@ -1,31 +1,17 @@
 <template>
 <div class="satellite">
-  <v-container>
-    <v-layout row wrap v-if="satellite">
-      <!-- description -->
+  <v-container v-if="satellite">
+    <v-layout row wrap>
+      <!-- column 1 -->
       <v-flex pa-4 mt-2 xs12 sm6 class="grey--text text--darken-1"> 
+        <!-- description -->
         <h1 class="heading">{{ satellite.displayName }}</h1>
         <p v-html="satellite.longDescription"></p>
         <ul>
           <li><strong>Status:</strong> {{satellite.status}}</li>
           <li><strong>Launch Date:</strong> {{formatLaunchDate(satellite.launchDate)}}</li>
         </ul>
-        
-      </v-flex>
-
-      <!-- satvis -->
-      <v-flex pa-4 mt-2 xs12 sm6 class="grey--text text--darken-1">
-        <v-card flat class="mr-5 my-3 pa-2 grey--text text--darken-3" style="min-height:500px;">
-          <iframe id="satvis-frame" allowtransparency="true" :src="`https://viewer.tinygs.com/?elements=Point,Label,Orbit,Sensor-cone,Ground-station-link&layers=OfflineHighres&tags=TinyGS&sat=${satellite.displayName}`" class="satvis" :class="{'satvis-fullScreen': satvisFullScreen, 'satvis-normal': !satvisFullScreen}"></iframe>
-          <div class="credits-satvis">Powered by <a href="https://github.com/Flowm/satvis">Satvis</a> (MIT)</div>
-          <v-btn @click="satvisFullScreen = !satvisFullScreen" class="mr-2" :class="{'fullScreen-btn-fullScreen': satvisFullScreen, 'fullScreen-btn-normal': !satvisFullScreen}" icon>
-            <v-img contain transition="fade-transition" max-height="60" src="../assets/fullScren-but.png" />
-          </v-btn>
-        </v-card>
-      </v-flex>
-
-      <v-flex pa-4 mt-2 xs12 sm6 class="grey--text text--darken-1"> 
-        <v-card v-if="satellite.tle" flat class="mr-5 my-3 pa-4 grey--text text--darken-3" style="overflow:auto;white-space: nowrap;">
+        <v-card v-if="satellite.tle && satellite.tle.length == 3" flat class="mr-5 my-3 pa-4 grey--text text--darken-3" style="overflow:auto;white-space: nowrap;">
           <h3>TLEs</h3>
           <v-card-text class="grey--text text--darken-3 mx-auto">
             <pre>
@@ -42,9 +28,22 @@
             <DynamicTemplate :packet="lastTelemetry" :customTemplate="satellite.webTemplate" />
           </v-card-text>
         </v-card>
+
+
       </v-flex>
-      <!-- images -->
+
+      <!-- Column 2 -->
       <v-flex pa-4 mt-2 xs12 sm6 class="grey--text text--darken-1">
+        <!-- satvis -->
+        <v-card flat class="mr-5 my-3 pa-2 grey--text text--darken-3" style="min-height:500px;">
+          <iframe id="satvis-frame" allowtransparency="true" :src="`https://viewer.tinygs.com/?elements=Point,Label,Orbit,Sensor-cone,Ground-station-link&layers=OfflineHighres&tags=TinyGS&sat=${satellite.displayName}`" class="satvis" :class="{'satvis-fullScreen': satvisFullScreen, 'satvis-normal': !satvisFullScreen}"></iframe>
+          <div class="credits-satvis">Powered by <a href="https://github.com/Flowm/satvis">Satvis</a> (MIT)</div>
+          <v-btn @click="satvisFullScreen = !satvisFullScreen" class="mr-2" :class="{'fullScreen-btn-fullScreen': satvisFullScreen, 'fullScreen-btn-normal': !satvisFullScreen}" icon>
+            <v-img contain transition="fade-transition" max-height="60" src="../assets/fullScren-but.png" />
+          </v-btn>
+        </v-card>
+
+        <!-- images -->
         <v-layout row wrap>
           <v-flex xs12 md6 v-for="image in satellite.images" :key="image">
               <v-card class="ma-2">
@@ -52,6 +51,7 @@
               </v-card>
           </v-flex>
         </v-layout>
+
       </v-flex>
       
       
@@ -60,25 +60,29 @@
         <line-chart height="200" width="400" v-if="datacollection" :chart-data="datacollection" :options="options"></line-chart>
         <button @click="fillData()">Randomize</button>
       </v-flex>-->
-      <!-- Packets -->
-      <v-flex xs12 sm12 pa-4 v-if="packets && packets.length > 0">
-        <div  v-for="packet in packets" :key="packet.id"> 
-          <PacketRow :packet="packet" :customTemplate="customTemplates[packet.satellite]" />
-          <v-divider></v-divider>
-        </div>
-      </v-flex>
-      <v-flex xs12 sm12 pa-4 v-else>
-        <div class="text-center grey--text text--darken-3">
-          No packets yet.
-        </div>
-      </v-flex>
     </v-layout>
-    <div v-else> <!-- loading spinner -->
-      <v-row class="fill-height ma-16" align="center" justify="center">
-        <v-progress-circular indeterminate color="grey"></v-progress-circular>
-      </v-row>
+      <!-- Packets -->
+      
+      <!--<h3>Packet list</h3>-->
+    <v-divider class="mt-6"></v-divider>
+    <div class="pa-4" v-if="packets && packets.length > 0">
+      <div  v-for="packet in packets" :key="packet.id"> 
+        <PacketRow :packet="packet" :customTemplate="customTemplates[packet.satellite]" />
+        <v-divider></v-divider>
+      </div>
+    </div>
+    <div class="pa-4" v-else>
+      <div class="text-center grey--text text--darken-3">
+        No packets yet.
+      </div>
     </div>
   </v-container>
+  <div v-else> <!-- loading spinner -->
+    <v-row class="fill-height ma-16" align="center" justify="center">
+      <v-progress-circular indeterminate color="grey"></v-progress-circular>
+    </v-row>
+  </div>
+  
 </div>
 </template>
 
